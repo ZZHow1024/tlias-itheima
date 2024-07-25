64ff5ebc22b99ccfdbcff4834a20d9665e873cac
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-07-25 21:38:07 +0800

    style: Add comments

--- src/main/java/com/zzhow/tliasitheima/controller/DeptController.java
+++ src/main/java/com/zzhow/tliasitheima/controller/DeptController.java
@@ -5,7 +5,9 @@ import com.zzhow.tliasitheima.pojo.Result;
 import com.zzhow.tliasitheima.service.DeptService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +25,10 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
+    /**
+     * 查询全部部门数据
+     * @return JSON全部部门数据
+     */
     //@RequestMapping(value = "depts", method = RequestMethod.GET)
     @GetMapping("depts")
     public Result list() {
